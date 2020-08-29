4e2589d6cd4f0f5d9f173a7b2f91f2e2fea7b7f8
Shubhangi Gupta <shubhigupta991@gmail.com>
2020-08-30 02:45:19 +0530

    Update

---
+++ DS - HashTables/Hashtables.py
@@ -0,0 +1,9 @@
+user = {
+    'name' : 'Shubhi',
+    'age' : 18,
+    'magic' : True,
+}
+
+print(user['age'])                      # O(1)
+user['magic_spell'] = 'AabraKaDabra'    # O(1)
+print(user)
