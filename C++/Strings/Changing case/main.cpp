f9313ff721727869a5e07eee1bba696325c9ab69
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-02-21 01:43:22 +0530

    Update

--- C++/Strings/Changing case/main.cpp
+++ C++/Strings/Changing case/main.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 int main() {
-    char* string = "welcome";
+    char string[] = "WelcomE";
     int i;
 
     cout << "Old string : " << string << '\n';
 
     for(i = 0; string[i] != '\0'; i++) {
-        string[i] = string[i] + 32;
+        if (string[i] >= 65 && string[i] <= 90)
+            string[i] += 32;
+        else if (string[i] >= 97 && string[i] <= 122)
+            string[i] -= 32;
     }
     cout << "New string : " << string;
 
