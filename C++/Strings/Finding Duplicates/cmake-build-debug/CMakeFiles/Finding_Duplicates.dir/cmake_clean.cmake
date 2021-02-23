6bfda2c4ce2e2c75c614281f4951393cca39a604
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-02-23 17:28:14 +0530

    Added Finding Duplicates

---
+++ C++/Strings/Finding Duplicates/cmake-build-debug/CMakeFiles/Finding_Duplicates.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Finding_Duplicates.dir/main.cpp.o"
+  "Finding_Duplicates"
+  "Finding_Duplicates.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Finding_Duplicates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/Strings/Finding Duplicates/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void duplicates1(char *S) {
+    for(int i = 0; S[i] != '\0'; i++) {
+        for(int j = i+1; S[j] != '\0'; j++){
+            if (S[i] == S[j])
+                cout << S[i] << '\t';
+        }
+    }
+}
+void duplicates2(char *S) {
+    int H[26];
+    for(int i = 0; i < 26; i++)
+        H[i] = 0;
+
+    for(int i = 0; S[i] != '\0'; i++)
+        H[S[i] - 97] += 1;
+
+    for(int i = 0; i < 26; i++) {
+        if (H[i] > 1)
+            cout << char(i+97) << " = " << H[i] <<'\t';
+    }
+}
+void duplicates3(char *S) {
+    long int H = 0, x = 0;
+    for(int i = 0; S[i] != '\0'; i++) {
+        x = 1;
+        x = x << S[i] - 97;
+
+        if ((x & H) > 0)
+            cout << S[i] << '\t';
+        else
+            H = x | H;
+    }
+}
+int main() {
+    char string[] = "finding";
+    duplicates1(string);
+    cout << endl;
+    duplicates2(string);
+    cout << endl;
+    duplicates3(string);
+    return 0;
+}
